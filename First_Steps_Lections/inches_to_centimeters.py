f424f7c6885e84ec18774edc6811810c02e5643f
Fevzi Babuch <fevzi.naile@gmail.com>
2024-09-17 16:49:31 +0200

    Uploading "First steps exercises" .py files

---
+++ First_Steps_Lections/inches_to_centimeters.py
@@ -0,0 +1,16 @@
+# Да се напише програма, която чете от конзолата реално число и го преобразува от инчове в сантиметри.
+# За целта умножете инчовете по 2.54 (1 инч = 2.54 сантиметра).
+
+real_number = float(input())
+
+conversion_value = (input())
+
+converted_number = float(real_number * conversion_value)
+
+print(converted_number)
+
+
+
+
+
+
